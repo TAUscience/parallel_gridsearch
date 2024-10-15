e5e3df628b8859937c3d993111f490636a55f617
TAUscience <tauscience15@gmail.com>
2024-10-15 12:10:26 -0600

    Data clean and split

--- data/load_data.py
+++ data/load_data.py
@@ -1,6 +1,15 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 def load_data():
-    #Hacer limpieza y separación de datos
+    data = pd.read_csv("data/cs-training.csv", na_values = "NA")
+    print(data.shape)
+    data.dropna(inplace = True)
+    print(data.shape)
 
-    "devolver X_train, X_test, y_train, y_test"
+    X = data.drop(columns = ["SeriousDlqin2yrs"])
+    y = data["SeriousDlqin2yrs"]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=777, stratify=y)
+
+    return X_train, X_test, y_train, y_test
