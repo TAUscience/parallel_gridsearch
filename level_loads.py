6e97814fc19022db46961a882e6af0afe698a54f
TAUscience <tauscience15@gmail.com>
2024-10-14 23:02:32 -0600

    Edit level_loads file

---
+++ level_loads.py
@@ -0,0 +1,14 @@
+def level_loads(D, n_p):
+    s = len(D)%n_p
+    n_D = D[:s]
+    t = int((len(D)-s)/n_p)
+    out=[]
+    temp=[]
+    for i in D[s:]:
+        temp.append(i)
+        if len(temp)==t:
+            out.append(temp)
+            temp = []
+    for i in range(len(n_D)):
+        out[i].append(n_D[i])
+    return out
